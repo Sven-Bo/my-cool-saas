0e6c9f172c85be20c94b8a26176d543198322341
Sven <67136317+Sven-Bo@users.noreply.github.com>
2023-08-06 20:51:26 +0200

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from st_paywall import add_auth
+
+st.set_page_config(layout="wide")
+st.title("The Most Fantabulous SaaS Ever! 🚀")
+
+add_auth(required=True)
+
+# ONLY AFTER THE AUTHENTICATION + SUBSCRIPTION, THE USER WILL SEE THIS ⤵
+# The email and subscription status is stored in session state.
+st.write(f"Subscription Status: {st.session_state.user_subscribed}")
+st.write("🎉 Yay! You're all set and subscribed! 🎉")
+st.write(f'By the way, your email is: {st.session_state.email}')
